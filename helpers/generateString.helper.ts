ac5d2f2bdd506ba9db499bcccdf69a9eb454f410
DimaSmaha <dmytrosmaha@gmail.com>
2025-03-19 21:40:34 +0200

    add helpers

---
+++ helpers/generateString.helper.ts
@@ -0,0 +1,17 @@
+import { getFakerData } from "./getFakerData.helper";
+
+export const generateRandomLogin = () => {
+  var length = 8,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+};
+
+export const userCheckoutDate = {
+  firstName: getFakerData.firstName(),
+  lastName: getFakerData.lastName(),
+  postalCode: getFakerData.postalCode(),
+};
---
+++ helpers/getEnvVariables.helper.ts
@@ -0,0 +1,4 @@
+export const getValidUserData = {
+  validUserLogin: process.env.VALID_USER_LOGIN as string,
+  validUserPassword: process.env.VALID_USER_PASSWORD as string,
+};
